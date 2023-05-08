8a95c1f09829b7e6c5aab8def0d8cc876d4334b3
Denis Fadeev <denis@fadeev.org>
2023-05-08 12:19:25 +0400

    feat: update faucet task to use new faucet

--- tasks/faucet.ts
+++ tasks/faucet.ts
@@ -1,23 +1,38 @@
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import * as dotenv from "dotenv";
-import axios from "axios";
+import { drip } from "@zetachain/faucet-cli/dist/commands/drip";
 
 dotenv.config();
 
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
-  if (!process.env.PRIVATE_KEY) {
-    throw new Error("Please set PRIVATE_KEY. Run: npx hardhat account --save");
+  let address = "";
+  if (args.address) {
+    address = args.address;
+  } else if (process.env.PRIVATE_KEY) {
+    address = new hre.ethers.Wallet(process.env.PRIVATE_KEY).address;
+    console.log(
+      `Using address derived from the PRIVATE_KEY env variable: ${address}`
+    );
+  } else {
+    console.error(
+      "❌ Error: please, provide an address as a flag or create a wallet with: npx hardhat account --save"
+    );
   }
-
-  const { address } = new hre.ethers.Wallet(process.env.PRIVATE_KEY);
-
-  try {
-    await axios.get(`https://faucet.zetachain.link/eth/${address}`);
-    console.log(`✅ Tokens requested successfully for ${address}`);
-  } catch (error) {
-    console.error("❌ Error requesting token from the faucet.");
-  }
+  await drip({ chain: args.chain, address }, []);
 };
 
-task("faucet", "").setAction(main);
+task(
+  "faucet",
+  `Request ZETA tokens from the faucet. If --address flag is not set, the faucet will default to the address derived from the wallet defined in PRIVATE_KEY env variable. If you don't have a wallet yet, use npx hardhat account --save to create a wallet in .env. Valid values for --chain flag are: zetachain_athens, goerli, bsc_testnet, polygon_mumbai. You can install a standalone version of the CLI faucet with: npm i -g @zetachain/faucet-cli`
+)
+  .addOptionalParam(
+    "address",
+    "Recipient address where tokens will be sent. (default: address derived from PRIVATE_KEY env variable)"
+  )
+  .addParam(
+    "chain",
+    "Blockchain network where tokens will be sent.",
+    "zetachain_athens"
+  )
+  .setAction(main);
