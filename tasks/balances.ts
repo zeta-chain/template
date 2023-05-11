fe0c013d715c5b1885ee50811f714712277f2906
Denis Fadeev <denis@fadeev.org>
2023-05-11 14:35:38 +0400

    feat: balances.ts

--- tasks/balances.ts
+++ tasks/balances.ts
@@ -8,43 +8,49 @@ import ZetaEth from "@zetachain/interfaces/abi/json/contracts/Zeta.eth.sol/ZetaE
 dotenv.config();
 
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
-  if (!process.env.PRIVATE_KEY) {
+  let address;
+  if (args.address) {
+    [address] = args;
+  } else if (process.env.PRIVATE_KEY) {
+    [address] = new hre.ethers.Wallet(process.env.PRIVATE_KEY);
+  } else {
     throw new Error("Please set PRIVATE_KEY. Run: npx hardhat account --save");
   }
 
-  const { address } = new hre.ethers.Wallet(process.env.PRIVATE_KEY);
-  let balances: any[] = [];
+  console.log(address);
 
-  for (const networkName in hre.config.networks) {
-    try {
-      const { url } = hre.config.networks[networkName];
-      const zetaAddress = getAddress({
-        address: "zetaToken",
-        networkName,
-        zetaNetwork: "athens",
-      });
-      const provider = new ethers.providers.JsonRpcProvider(url);
+  //   let balances: any[] = [];
 
-      const nb = await provider.getBalance(address);
-      const native = parseFloat(ethers.utils.formatEther(nb)).toFixed(2);
+  //   for (const networkName in hre.config.networks) {
+  //     try {
+  //       const { url } = hre.config.networks[networkName];
+  //       const zetaAddress = getAddress({
+  //         address: "zetaToken",
+  //         networkName,
+  //         zetaNetwork: "athens",
+  //       });
+  //       const provider = new ethers.providers.JsonRpcProvider(url);
 
-      const zetaContract = new ethers.Contract(zetaAddress, ZetaEth, provider);
-      const zb = await zetaContract.balanceOf(address);
-      const zeta = parseFloat(ethers.utils.formatEther(zb)).toFixed(2);
+  //       const nb = await provider.getBalance(address);
+  //       const native = parseFloat(ethers.utils.formatEther(nb)).toFixed(2);
 
-      balances.push({ networkName, native, zeta });
-    } catch (error) {
-      continue;
-    }
-  }
+  //       const zetaContract = new ethers.Contract(zetaAddress, ZetaEth, provider);
+  //       const zb = await zetaContract.balanceOf(address);
+  //       const zeta = parseFloat(ethers.utils.formatEther(zb)).toFixed(2);
 
-  console.log(`
-📊 Balances for ${address}
-`);
-  console.table(balances);
+  //       balances.push({ networkName, native, zeta });
+  //     } catch (error) {
+  //       continue;
+  //     }
+  //   }
+
+  //   console.log(`
+  // 📊 Balances for ${address}
+  // `);
+  //   console.table(balances);
 };
 
-const taskDesc = "Fetch native and ZETA token balances";
-const addressDesc = "Fetch balances for a specific address";
+const descTask = "Fetch native and ZETA token balances";
+const descAddressFlag = "Fetch balances for a specific address";
 
-task("balances", taskDesc, main).addOptionalParam("address", addressDesc);
+task("balances", descTask, main).addOptionalParam("address", descAddressFlag);
