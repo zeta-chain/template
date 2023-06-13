3776e741d2306df4c7a33ee98bf44467bebc5782
Denis Fadeev <denis@fadeev.org>
2023-06-13 16:20:56 +0400

    refactor: import tasks and config from deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
-import { getHardhatConfigNetworks } from "@zetachain/addresses-tools/dist/networks";
-
-import "./tasks/account";
-import "./tasks/faucet";
-import "./tasks/balances";
-import "./tasks/verify";
-
-dotenv.config();
-const PRIVATE_KEYS =
-  process.env.PRIVATE_KEY !== undefined ? [`0x${process.env.PRIVATE_KEY}`] : [];
+import { getHardhatConfigNetworks } from "@zetachain/networks";
+import "@zetachain/toolkit/tasks";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
-    ...getHardhatConfigNetworks(PRIVATE_KEYS),
+    ...getHardhatConfigNetworks(),
   },
 };
 
