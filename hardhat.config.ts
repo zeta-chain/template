262e796b791ad291b790929a85dd65f06e5888a7
Denis Fadeev <denis@fadeev.org>
2023-06-26 12:25:47 +0400

    bug: getAddress returning undefined

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 import { getHardhatConfigNetworks } from "@zetachain/addresses-tools/dist/networks";
+import { getAddress } from "@zetachain/addresses";
 
 import "./tasks/account";
 import "./tasks/faucet";
@@ -11,6 +12,14 @@ import "./tasks/verify";
 dotenv.config();
 const PRIVATE_KEYS =
   process.env.PRIVATE_KEY !== undefined ? [`0x${process.env.PRIVATE_KEY}`] : [];
+
+console.log(
+  getAddress({
+    address: "zetaTokenConsumerUniV3",
+    networkName: "polygon-mumbai",
+    zetaNetwork: "athens",
+  })
+);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
