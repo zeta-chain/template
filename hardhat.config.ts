bce2ae34407fa0ecfc0b92a0d406307fe8a66594
Denis Fadeev <denis@fadeev.org>
2023-08-18 11:27:55 +0400

    dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,14 @@
 import "@nomicfoundation/hardhat-toolbox";
-import { getHardhatConfigNetworks } from "@zetachain/networks";
 import "@zetachain/toolkit/tasks";
+
+import { getHardhatConfigNetworks } from "@zetachain/networks";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.7",
   networks: {
     ...getHardhatConfigNetworks(),
   },
+  solidity: "0.8.7",
 };
 
 export default config;
